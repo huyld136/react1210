69f0ddbee82f67526d460a9288d82d0a85fbed7f
huyld <huyld136@gmail.com>
2020-10-23 21:53:53 +0700

    them_buoi08_lifeCycle

--- src/FormComponent/DanhSachNguoiDung.js
+++ src/FormComponent/DanhSachNguoiDung.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react'
 import FormComponent from './FormComponent'
+import { connect } from 'react-redux'
+import { xoaNguoiDungAction, suaNguoiDungAction } from '../redux/actions/QuanLyNguoiDungActions';
+import NewFormComponent from './NewFormComponent';
 
-export default class DanhSachNguoiDung extends Component {
+class DanhSachNguoiDung extends Component {
     render() {
+
+
         return (
             <div className="container mt-5">
-                <FormComponent />
+                {/* <FormComponent /> */}
+                <NewFormComponent />
                 <table className="table mt-5">
                     <thead className="bg-dark text-light font-weight-bold">
                         <tr>
@@ -17,10 +23,42 @@ export default class DanhSachNguoiDung extends Component {
                         </tr>
                     </thead>
                     <tbody>
+                        {this.props.QuanLyNguoiDungReducer.map((item, index) => {
+                            return (
+                                <tr key={index}>
+                                    <td>{item.maNguoiDung}</td>
+                                    <td>{item.tenNguoiDung}</td>
+                                    <td>{item.soDienThoai}</td>
+                                    <td>{item.email}</td>
+                                    <td>
+                                        <button className="btn btn-primary mr-3" onClick={() => {
+                                            this.props.dispatch(suaNguoiDungAction(item))
+                                        }}>Sửa</button>
+                                        <button className="btn btn-danger" onClick={() => {
+                                            //Gọi hàm trong mapDispatchTopProps
 
+                                            //Hàm này khi sử dụng connect với redux sẽ tự có props này
+                                            // let action = {
+                                            //     type: 'XOA_NGUOI_DUNG',
+                                            //     maNguoiDung: item.mangNguoiDung,
+                                            // }
+                                            this.props.dispatch(xoaNguoiDungAction(item.maNguoiDung));
+                                        }}>Xóa</button>
+                                    </td>
+                                </tr>
+                            );
+                        })}
                     </tbody>
                 </table>
             </div >
         )
     }
 }
+
+const mapStateToProps = (state) => {
+    return {
+        QuanLyNguoiDungReducer: state.QuanLyNguoiDungReducer.mangNguoiDung,
+    }
+}
+
+export default connect(mapStateToProps)(DanhSachNguoiDung)
--- src/redux/reducer/rootReducer.js
+++ src/redux/reducer/rootReducer.js
@@ -2,10 +2,12 @@
 import { combineReducers } from 'redux'
 import BaiTapGioHangReducer from './BaiTapGioHangReducer'
 import baiTapGameXucXacReducer from './BaiTapGameXucXacReducer'
+import {QuanLyNguoiDungReducer} from './QuanLyNguoiDungReducer'
 
 // state tổng của ứng dụng
 export const rootReducer = combineReducers({
     // Nơi khai báo các state theo từng nghiệp vụ
     StateBaiTapGioHang: BaiTapGioHangReducer,
     stateBaiTapGameXucXac: baiTapGameXucXacReducer,
+    QuanLyNguoiDungReducer: QuanLyNguoiDungReducer,
 });
